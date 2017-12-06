c24ed100905d70c8b6e05747b08c6d0841a3f8f4
garicchi <xgaricchi@gmail.com>
2017-12-07 08:51:45 +0900

    Create japanese_demo.py

---
+++ japanese_demo.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import aiy.audio
+import aiy.cloudspeech
+import aiy.voicehat
+import aiy.i18n
+
+def main():
+    # 言語設定を日本語にする
+    aiy.i18n.set_language_code('ja-JP')
+    recognizer = aiy.cloudspeech.get_recognizer()
+
+    button = aiy.voicehat.get_button()
+    led = aiy.voicehat.get_led()
+    aiy.audio.get_recorder().start()
+
+    while True:
+        print('Press the button and speak')
+        button.wait_for_press()
+        aiy.audio.say('ご用件をどうぞ')
+        print('Listening...')
+        text = recognizer.recognize()
+        if text is None:
+            print('Sorry, I did not hear you.')
+        else:
+            print('You said "', text, '"')
+            if '点灯' in text:
+                aiy.audio.say('ライトをつけました')
+                led.set_state(aiy.voicehat.LED.ON)
+            elif '終了' in text:
+                break
+
+
+if __name__ == '__main__':
+    main()
